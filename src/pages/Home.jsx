4cb1083ffc95498f8bf4831fd017e6738b1741da
Balram Patidar <pbalram088@gmail.com>
2022-06-10 21:37:39 +0530

    auth app
    auth app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,15 +9,15 @@ const Home = () => {
     });
   }, []);
   return (
-    <div>
-      <h1>Home</h1>
-      <br />
-      {feeds.map((feed) => (
-        <div
-          key={feed.id}
-          style={{
-            padding: "10px",
-            margin: "auto",
+   <div>
+     <h1>Home</h1>
+     <br />
+     {feeds.map((feed) => (
+       <div
+         key={feed.id}
+         style={{
+           padding: "10px",
+           margin: "auto",
             marginTop: "10px",
             border: "1px solid grey",
             maxWidth: "200px",
